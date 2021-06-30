4566769f33eff9e5fa815a06aaf3c60a3ec14306
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-06-30 10:01:28 +0000

    Update Python publisher to output InputPressure messages with step function

--- scripts/python_pub.py
+++ scripts/python_pub.py
@@ -1,37 +1,48 @@
+import numpy as np
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from vtem_control.msg import InputPressures
 
 
 class VtemControlPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__('vtem_control_publisher')
+        self.publisher_ = self.create_publisher(InputPressures, 'topic', 10)
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        pressure_scalar = self.pressure_step(self.i)
+        pressure_array = pressure_scalar * 100 * np.ones(shape=(4,))
+
+        msg = InputPressures()
+        msg.data = list(pressure_array)
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info(f'Publishing msg {self.i}: {msg.data}')
         self.i += 1
+
+    def pressure_step(self, i: int) -> int:
+        if i % 1500 < 750:
+            return 300 # mBar
+        else:
+            return 450 # mBar
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = VtemControlPublisher()
+    vtem_control_publisher = VtemControlPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(vtem_control_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    vtem_control_publisher.destroy_node()
     rclpy.shutdown()
 
 
