1b31c24e4e52b2bf64ffae3e5d3a0a48d6392a68
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-06-30 20:05:25 +0000

    Use FluidPressure template for input pressures

--- scripts/python_pub_node.py
+++ scripts/python_pub_node.py
@@ -2,7 +2,8 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 
-from vtem_control.msg import InputPressures
+from vtem_control.msg import FluidPressures
+from sensor_msgs.msg import FluidPressure
 
 
 class VtemControlPublisher(Node):
@@ -13,7 +14,7 @@ class VtemControlPublisher(Node):
         self.declare_parameter('vtem_input_pressures_topic', 'vtem_input_pressures')
 
         vtem_input_pressures_topic = self.get_parameter('vtem_input_pressures_topic').get_parameter_value().string_value
-        self.publisher_ = self.create_publisher(InputPressures, vtem_input_pressures_topic, 10)
+        self.publisher_ = self.create_publisher(FluidPressures, vtem_input_pressures_topic, 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -22,8 +23,14 @@ class VtemControlPublisher(Node):
         pressure_scalar = self.pressure_step(self.i)
         pressure_array = pressure_scalar * 100 * np.ones(shape=(4,))
 
-        msg = InputPressures()
-        msg.data = list(pressure_array)
+        msg = FluidPressures()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.data = []
+        for pressure in pressure_array:
+            fluid_pressure_msg = FluidPressure()
+            fluid_pressure_msg.header.stamp = self.get_clock().now().to_msg()
+            fluid_pressure_msg.fluid_pressure = pressure
+            msg.data.append(fluid_pressure_msg)
 
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing msg {self.i}: {msg.data}')
--- src/pressure_regulator_sub_node.cpp
+++ src/pressure_regulator_sub_node.cpp
@@ -3,7 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "VtemControl.hpp"
-#include "vtem_control/msg/input_pressures.hpp"
+#include "vtem_control/msg/fluid_pressures.hpp"
 using std::placeholders::_1;
 
 class InputPressuresSubscriber : public rclcpp::Node
@@ -24,7 +24,7 @@ public:
     this->get_parameter("vtem_node", vtem_node_);
     this->get_parameter("vtem_service", vtem_service_);
 
-    subscription_ = this->create_subscription<vtem_control::msg::InputPressures>(
+    subscription_ = this->create_subscription<vtem_control::msg::FluidPressures>(
       vtem_input_pressures_topic_.c_str(), 10, std::bind(&InputPressuresSubscriber::topic_callback, this, _1));
     
     // Create VtemControl object
@@ -40,15 +40,19 @@ public:
   }
 
 private:
-  void topic_callback(const vtem_control::msg::InputPressures::SharedPtr msg)
+  void topic_callback(const vtem_control::msg::FluidPressures::SharedPtr msg)
   {
-    RCLCPP_INFO(this->get_logger(), "I received msg with pressure[0]: %d mBar", (int) (msg->data[0]/100));
-    std::vector<float> input_pressures(msg->data.begin(), msg->data.end());
-    std::transform(input_pressures.begin(), input_pressures.end(), input_pressures.begin(), [](float &c){ return c / 100;});
-    std::vector<int> input_pressures_mbar(input_pressures.begin(), input_pressures.end());
+    RCLCPP_INFO(this->get_logger(), "I received msg with pressure[0]: %d mBar", (int) (msg->data[0].fluid_pressure/100));
+    std::vector<int> input_pressures_mbar(msg->data.size(), 0);
+    int idx = 0;
+    for (auto it = msg->data.begin(); it != msg->data.end(); ++it) {
+      sensor_msgs::msg::FluidPressure fluid_pressure_msg =  *it;
+      input_pressures_mbar[idx] = (int) fluid_pressure_msg.fluid_pressure / 100;
+      idx += 1;
+    }
     vtemControl_.set_all_pressures(input_pressures_mbar);
   }
-  rclcpp::Subscription<vtem_control::msg::InputPressures>::SharedPtr subscription_;
+  rclcpp::Subscription<vtem_control::msg::FluidPressures>::SharedPtr subscription_;
 
   std::string vtem_input_pressures_topic_;
   std::string vtem_node_;
