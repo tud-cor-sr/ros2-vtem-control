b707693b273286c2d226f3ff5cc238d1baf54f4d
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-29 16:49:45 +0200

    Add necessary adjustments to Matlab ROS input pressure subscription node

--- vtem_control_matlab/input_pressures_sub_node.m
+++ vtem_control_matlab/input_pressures_sub_node.m
@@ -6,9 +6,14 @@ global vtemControl
 vtemControl = VtemControl(vtemDeviceAddress, vtemPort);
 vtemControl.connect();
 
+vtemControl.acknowledge_errors_all_slots();
+vtemControl.activate_pressure_regulation_all_slots();
+
 % ROS node initiated by Matlab
 inputPressuresSubNode = ros2node("vtem_control/input_pressures_sub_node");
 inputPressuresSub = ros2subscriber(inputPressuresSubNode, "/vtem_control/input_pressures", @input_pressures_callback);
+
+vtemControl.deactivate_pressure_regulation_all_slots();
 
 function input_pressures_callback(msg)
     pressures = FluidPressures_msg_to_array(msg)*10^(-2); % [Pa] to [mBar]
