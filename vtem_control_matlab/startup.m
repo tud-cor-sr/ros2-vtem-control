6459e197a7c818412ab6022bf73297a571d5c5bf
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-09-29 21:23:38 +0200

    Re-order commands in startup

--- vtem_control_matlab/startup.m
+++ vtem_control_matlab/startup.m
@@ -1,10 +1,10 @@
+% parameters
+vtemDeviceAddress = "192.168.4.3";
+vtemPort = 502;
+
 % set RMW Implementation
 setenv('RMW_IMPLEMENTATION','')
 
 % load custom messages
 pkgFolderPath = fullfile(pwd, "../");
 ros2genmsg(pkgFolderPath)
-
-% parameters
-vtemDeviceAddress = "192.168.4.3";
-vtemPort = 502;
