e4d56a6652bd1c0cb47d7979086c78711b9118cb
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-11-25 08:20:54 +0000

    Publish measured valve pressures as array as well

--- vtem_control_cpp/src/output_pressures_pub_node.cpp
+++ vtem_control_cpp/src/output_pressures_pub_node.cpp
@@ -1,8 +1,10 @@
+#include <functional> 
 #include <memory>
 #include <stdexcept>
 
 #include "rclcpp/rclcpp.hpp"
 #include "pneumatic_actuation_msgs/msg/fluid_pressures.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 #include "VtemControl.hpp"
 
 using namespace std::chrono_literals;
@@ -13,13 +15,10 @@ public:
   OutputPressuresPub()
   : Node("output_pressures_pub")
   {
-    // VTEM input pressures topic
-    this->declare_parameter<std::string>("output_pressures_topic", "output_pressures");
-    this->get_parameter("output_pressures_topic", vtem_output_pressures_topic_);
-
     // publication frequency
     this->declare_parameter<float>("pub_freq", 50.);
     this->get_parameter("pub_freq", pub_freq_);
+    timer_ = this->create_wall_timer(std::chrono::microseconds((int) (1000000 / pub_freq_)), std::bind(&OutputPressuresPub::timer_callback, this));
 
     // VTEM modbus network information
     this->declare_parameter<std::string>("modbus_node", "192.168.4.3");
@@ -31,8 +30,14 @@ public:
     this->declare_parameter<int>("num_valves", 16);
     this->get_parameter("num_valves", num_valves_);
 
+    // VTEM output pressures topic
+    this->declare_parameter<std::string>("output_pressures_topic", "output_pressures");
+    this->get_parameter("output_pressures_topic", vtem_output_pressures_topic_);
+    this->declare_parameter<std::string>("output_pressures_array_topic", "output_pressures_array");
+    this->get_parameter("output_pressures_array_topic", vtem_output_pressures_array_topic_);
+
     publisher_ = this->create_publisher<pneumatic_actuation_msgs::msg::FluidPressures>(vtem_output_pressures_topic_.c_str(), 10);
-    timer_ = this->create_wall_timer(std::chrono::microseconds((int) (1000000 / pub_freq_)), std::bind(&OutputPressuresPub::timer_callback, this));
+    publisher_array_ = this->create_publisher<std_msgs::msg::Float64MultiArray>(vtem_output_pressures_array_topic_.c_str(), 10);
     
     // Create VtemControl object
     this->vtemControl_ = std::unique_ptr<vtem_control::VtemControl>(new vtem_control::VtemControl(modbus_node_.c_str(), modbus_service_.c_str(), num_valves_));
@@ -57,25 +62,43 @@ private:
     msg.header.stamp = OutputPressuresPub::get_clock()->now();
     std::vector<sensor_msgs::msg::FluidPressure> fluid_pressure_msgs(output_pressures_mbar.size());
 
+    auto msg_multi_array = std_msgs::msg::Float64MultiArray();
+    msg_multi_array.layout = std_msgs::msg::MultiArrayLayout();
+    msg_multi_array.layout.dim.push_back(std_msgs::msg::MultiArrayDimension());
+    msg_multi_array.layout.dim.push_back(std_msgs::msg::MultiArrayDimension());
+    msg_multi_array.layout.dim[0].label = "segments";
+    msg_multi_array.layout.dim[1].label = "chambers";
+
     int idx = 0;
+    std::vector<double> output_pressures(output_pressures_mbar.size());
     for (auto it = output_pressures_mbar.begin(); it != output_pressures_mbar.end(); ++it) {
+      double pressure = (double) *it * 100;
+
+      output_pressures[idx] = pressure;
+
       sensor_msgs::msg::FluidPressure fluid_pressure_msg;
       fluid_pressure_msg.header.stamp =  OutputPressuresPub::get_clock()->now();
-      fluid_pressure_msg.fluid_pressure =  (float) *it * 100;
+      fluid_pressure_msg.fluid_pressure =  pressure;
       fluid_pressure_msgs[idx] = fluid_pressure_msg;
+
       idx += 1;
     }
     msg.data = fluid_pressure_msgs;
+    msg_multi_array.data = output_pressures;
 
     publisher_->publish(msg);
+    publisher_array_->publish(msg_multi_array);
 
     RCLCPP_INFO(this->get_logger(), "I published FluidPressures with actual pressure [0]: %d mBar", (int) (msg.data[0].fluid_pressure/100));
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<pneumatic_actuation_msgs::msg::FluidPressures>::SharedPtr publisher_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher_array_;
   size_t count_;
 
   std::string vtem_output_pressures_topic_;
+  std::string vtem_output_pressures_array_topic_;
+
   float pub_freq_;
   std::string modbus_node_;
   std::string modbus_service_;
